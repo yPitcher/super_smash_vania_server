9c5309418bf86d60d8a077851b6c1a92210c8945
yPitcher <50935241+yPitcher@users.noreply.github.com>
2022-11-29 05:00:51 -0300

    Feature: Sync movements by RPC
    +Sync movements with server via RPC

--- Server.gd
+++ Server.gd
@@ -2,9 +2,14 @@ extends Node
 
 var ENet = NetworkedMultiplayerENet.new()
 
-var port : int = 3033
+var port : int = 42069
 #Número máximo de conexões simultâneas
 var max_jogadores : int = 2
+
+var players = {}
+var self_data = { id="", position=Vector2(300,60) }
+
+const SPEED = 300
 
 func _ready():
 	StartServer()
@@ -23,3 +28,18 @@ func _conecta_jogador(player_id):
 	
 func _discon_jogador(player_id):
 	print(str(player_id), ' desconectou')
+	
+	
+# <remote> é uma keyword para indicar que a função é executada remotamente,
+# ou seja, um cliente pode chamar esta função.
+# Geralmente é utilizada em conjunto da função rpc_id(...)
+
+# <sync> é outra keyword bem utilizada para sincronizar os clientes com o servidor.
+# Geralmente é utilizada em conjunto da função rpc(...)
+remote func _get_player_pos(var motion, var direction):
+	
+	motion.x = direction * SPEED
+	
+	var sender_id = get_tree().get_rpc_sender_id()
+	rpc_id(sender_id, "_get_player_pos", motion, direction)
+	print('Enviando [', motion, '] para o cliente [', sender_id,']')
